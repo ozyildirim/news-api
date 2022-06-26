6978708e2f2690a40200ad7b84cbbf4aa3d9e7fb
Ömer Kutay Yıldırım <kutay@gelal.com>
2022-06-27 00:06:49 +0300

    added new sources

--- index.js
+++ index.js
@@ -8,22 +8,70 @@ const app = express();
 
 
 const newspapers = [{
+        name: 'cityam',
+        address: 'https://www.cityam.com/london-must-become-a-world-leader-on-climate-change-action/',
+        base: ''
+    },
+    {
         name: 'thetimes',
         address: 'https://www.thetimes.co.uk/environment/climate-change',
         base: ''
-    }, {
+    },
+    {
         name: 'guardian',
         address: 'https://www.theguardian.com/environment/climate-crisis',
-        base: ''
-
+        base: '',
     },
     {
         name: 'telegraph',
         address: 'https://www.telegraph.co.uk/climate-change',
-        base: 'https://www.telegraph.co.uk'
+        base: 'https://www.telegraph.co.uk',
     },
-
-
+    {
+        name: 'nyt',
+        address: 'https://www.nytimes.com/international/section/climate',
+        base: '',
+    },
+    {
+        name: 'latimes',
+        address: 'https://www.latimes.com/environment',
+        base: '',
+    },
+    {
+        name: 'smh',
+        address: 'https://www.smh.com.au/environment/climate-change',
+        base: 'https://www.smh.com.au',
+    },
+    {
+        name: 'un',
+        address: 'https://www.un.org/climatechange',
+        base: '',
+    },
+    {
+        name: 'bbc',
+        address: 'https://www.bbc.co.uk/news/science_and_environment',
+        base: 'https://www.bbc.co.uk',
+    },
+    {
+        name: 'es',
+        address: 'https://www.standard.co.uk/topic/climate-change',
+        base: 'https://www.standard.co.uk'
+    },
+    {
+        name: 'sun',
+        address: 'https://www.thesun.co.uk/topic/climate-change-environment/',
+        base: ''
+    },
+    {
+        name: 'dm',
+        address: 'https://www.dailymail.co.uk/news/climate_change_global_warming/index.html',
+        base: ''
+    },
+    {
+        name: 'nyp',
+        address: 'https://nypost.com/tag/climate-change/',
+        base: ''
+    }
 ];
 
 const articles = [];
